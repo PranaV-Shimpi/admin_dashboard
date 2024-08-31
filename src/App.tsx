d2852688ca163e934cbe65b187ba9a8179f6650a
PranaV Shimpi <23pranavshimpi@gmail.com>
2024-08-31 17:18:08 +0530

    update: initial Hello world, clean code

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,55 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { RefineThemes, useNotificationProvider } from "@refinedev/antd";
+import { Refine } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
-import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
-
-import { useNotificationProvider } from "@refinedev/antd";
-import "@refinedev/antd/dist/reset.css";
-
-import dataProvider, {
-  GraphQLClient,
-  liveProvider,
-} from "@refinedev/nestjs-query";
-import routerBindings, {
+import routerProvider, {
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
-import { App as AntdApp } from "antd";
-import { createClient } from "graphql-ws";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { authProvider } from "./authProvider";
-import { ColorModeContextProvider } from "./contexts/color-mode";
 
-const API_URL = "https://api.nestjs-query.refine.dev/graphql";
-const WS_URL = "wss://api.nestjs-query.refine.dev/graphql";
+import { App as AntdApp, ConfigProvider } from "antd";
 
-const gqlClient = new GraphQLClient(API_URL);
-const wsClient = createClient({ url: WS_URL });
+import { authProvider, dataProvider, liveProvider } from "./providers";
 
-function App() {
+import "@refinedev/antd/dist/reset.css";
+
+import {ForgotPassword, Home, Login, Register} from './pages'
+
+const App = () => {
   return (
     <BrowserRouter>
-      <GitHubBanner />
-      <RefineKbarProvider>
-        <ColorModeContextProvider>
-          <AntdApp>
-            <DevtoolsProvider>
-              <Refine
-                dataProvider={dataProvider(gqlClient)}
-                liveProvider={liveProvider(wsClient)}
-                notificationProvider={useNotificationProvider}
-                routerProvider={routerBindings}
-                authProvider={authProvider}
-                options={{
-                  syncWithLocation: true,
-                  warnWhenUnsavedChanges: true,
-                  useNewQueryKeys: true,
-                  projectId: "fVmWE7-qijxMX-Hk0XRN",
-                  liveMode: "auto",
-                }}
-              >
-                <Routes>
-                  <Route index element={<WelcomePage />} />
-                </Routes>
-                <RefineKbar />
-                <UnsavedChangesNotifier />
-                <DocumentTitleHandler />
-              </Refine>
-              <DevtoolsPanel />
-            </DevtoolsProvider>
-          </AntdApp>
-        </ColorModeContextProvider>
-      </RefineKbarProvider>
+      <ConfigProvider theme={RefineThemes.Blue}>
+        <AntdApp>
+          <DevtoolsProvider>
+            <Refine
+              routerProvider={routerProvider}
+              dataProvider={dataProvider}
+              liveProvider={liveProvider}
+              notificationProvider={useNotificationProvider}
+              authProvider={authProvider}
+              options={{
+                syncWithLocation: true,
+                warnWhenUnsavedChanges: true,
+                liveMode: "auto",
+                useNewQueryKeys: true,
+              }}
+            >
+              <Routes>
+                <Route index element={<Home />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login/>} />
+                <Route path="/forgot-password" element={<ForgotPassword />} />
+              </Routes>
+              <UnsavedChangesNotifier />
+              <DocumentTitleHandler />
+            </Refine>
+           
+          </DevtoolsProvider>
+        </AntdApp>
+      </ConfigProvider>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
